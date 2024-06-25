5792c9e6b18b38bc422f819e0eb155d388b81024
QuyAnhJJ <135755635+QuyAnhJJ@users.noreply.github.com>
2024-06-25 22:26:46 +0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,4 +2,13 @@
 require("@nomiclabs/hardhat-ethers");
 module.exports = {
   solidity: "0.8.17",
+  defaultNetwork: "bscTestnet",
+  networks: {
+    bscTestnet: {
+      url: "https://bsc-testnet-rpc.publicnode.com",
+      chainId: 97,
+      accounts: [`0x02f1e4727b5cb16aa9c9efd25b90f2d740a46bdbceabe9a7dd5e691701c3402c`],
+      gasPrice: 1e10
+    },
+  },
 };
