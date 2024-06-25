40bfd36a4363744112e12fe2398772280419066d
QuyAnhJJ <135755635+QuyAnhJJ@users.noreply.github.com>
2024-06-25 22:32:25 +0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,7 +7,7 @@ module.exports = {
     bscTestnet: {
       url: "https://bsc-testnet-rpc.publicnode.com",
       chainId: 97,
-      accounts: [`0x02f1e4727b5cb16aa9c9efd25b90f2d740a46bdbceabe9a7dd5e691701c3402c`],
+      accounts: [`0X[PRIVATE_KEY]'],
       gasPrice: 1e10
     },
   },
