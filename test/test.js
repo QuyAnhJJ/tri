b486f49e6b467ab0876806c86d30ec1e5bc0360f
QuyAnhJJ <135755635+QuyAnhJJ@users.noreply.github.com>
2024-06-25 22:16:10 +0700

    Add files via upload

---
+++ test/test.js
@@ -0,0 +1,59 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+describe('Splitwise Contract', function () {
+  let Splitwise, splitwise, owner, addr1, addr2, addr3;
+
+  beforeEach(async function () {
+    Splitwise = await ethers.getContractFactory('Splitwise');
+    [owner, addr1, addr2, addr3] = await ethers.getSigners();
+    splitwise = await Splitwise.deploy();
+    await splitwise.deployed();
+  });
+
+  it('should add a debt successfully', async function () {
+    const amount = 100;
+    await splitwise.add_IOU(addr1.address, amount, [], 0);
+    const debt = await splitwise.lookup(owner.address, addr1.address);
+    expect(debt).to.equal(amount);
+  });
+
+  it('should not allow adding debt to oneself', async function () {
+    await expect(splitwise.add_IOU(owner.address, 100, [], 0))
+      .to.be.revertedWith('Debtor equal to creditor!');
+  });
+
+  it('should lookup the correct debt amount', async function () {
+    const amount = 200;
+    await splitwise.add_IOU(addr1.address, amount, [], 0);
+    const debt = await splitwise.lookup(owner.address, addr1.address);
+    expect(debt).to.equal(amount);
+  });
+  it('should return zero for initial debts', async function () {
+    
+    const debtOwnerToAddr1 = await splitwise.lookup(owner.address, addr1.address);
+    const debtAddr1ToAddr2 = await splitwise.lookup(addr1.address, addr2.address);
+    const debtAddr2ToOwner = await splitwise.lookup(addr2.address, owner.address);
+
+    expect(debtOwnerToAddr1).to.equal(0);
+    expect(debtAddr1ToAddr2).to.equal(0);
+    expect(debtAddr2ToOwner).to.equal(0);
+  });
+  it('should correctly reflect debts after adding IOUs', async function () {
+    
+    await splitwise.add_IOU(addr1.address, 100, [], 0); 
+    await splitwise.connect(addr1).add_IOU(addr2.address, 200, [], 0); 
+
+    // Lookup the debts after adding IOUs
+    const debtOwnerToAddr1 = await splitwise.lookup(owner.address, addr1.address);
+    const debtAddr1ToAddr2 = await splitwise.lookup(addr1.address, addr2.address);
+
+    expect(debtOwnerToAddr1).to.equal(100); 
+    expect(debtAddr1ToAddr2).to.equal(200); 
+  });
+  
+  
+
+
+  
+});
